08d7c9a2f4e4f36058555b627a8fa1f5e793b2ed
Nick Yeung <dmsnick88@gmail.com>
2015-03-26 21:15:24 -0400

    hello

---
+++ test/Assets/sphereLerper.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	if(Input.anyKeyDown)
+		MoveOver();
+}
+
+function MoveOver () {
+	var t : float = 0;
+	while(t<1){
+		// startPoint * (1-t) + endPoint * t
+		transform.position = Vector3.Lerp(Vector3(-5,0,0), Vector3(5,0,0),t);
+		//transform.position = startPoint * (1-Mathf.Pow(t,2)) + endPoint *
+		t += 0.1*Time.deltaTime;
+		yield;
+	}
+}
